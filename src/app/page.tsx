25912b8fa5c23523de287fc238997ae346da6113
humna <humnamuzammil54@gmail.com>
2024-12-09 21:39:01 +0500

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,23 @@ import SignUp from './components/SignUp'
 import SignIn from "./components/SignIn"
 import Page4 from "./components/Page4"
 import ShoppingCart from './components/ShoppingCart'
+import Faq from './components/Faq'
+import Checkout from './components/Checkout'
+
 
 function page() {
   return (
     <>
  <MainContent /> 
      <Menu /> 
+     <ShoppingCart />
     <Ourchefs /> 
-   <ShoppingCart />
    <SignUp /> 
     <SignIn />
     <Page4 />
+   <Faq /> 
+   <Checkout />
+   
     </>
   )
 }
